6e00612d57e7997a32aa88c90e1008572a76a074
Konsta <konsta09@gmail.com>
2013-03-25 09:43:37 +0200

    modify makefiles for qcom legacy

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -7,4 +8,5 @@ endif
 ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+endif
 endif
